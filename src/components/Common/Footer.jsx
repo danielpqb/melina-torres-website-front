912b1fddad43707e2295900daa2fffabcb094311
danielpqb <danielpqb@gmail.com>
2022-09-22 23:03:07 -0300

    First look

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Icon from "./Icon";
 import Logo from "./Logo";
